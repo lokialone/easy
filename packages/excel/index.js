c3b017794615b6ef3c566a2ac6941f3ffb8c6d88
loki <648875862@qq.com>
2019-10-04 19:29:50 +0800

    feat: 完成excel 数据过滤, 然而没有很好的可以生成有下拉选项的excel文件的js库,淦

---
+++ packages/excel/index.js
@@ -0,0 +1,127 @@
+// import XLSX from 'xlsx';
+const XLSX = require('xlsx');
+const workbook = XLSX.readFile('/Users/lokalone/Downloads/export.xls');
+// console.log(workbook);
+let sheetNames = workbook.SheetNames;
+let worksheet = workbook.Sheets[sheetNames[0]];
+console.log(worksheet['A1'])
+console.log(worksheet['A2'])
+const salerName = '业务员名称';
+const bussinessAddress = '办公地址';
+const billAddress = '开票地址';
+
+// const filterOut = [
+//     {
+//         columnName: '办公地址',
+//         test: '测试'
+//     },
+//     {
+//         columnName: '开票地址',
+//         test: '测试'
+//     }
+
+// ];
+const filterIn = [
+
+]
+
+function alphabetNext(a) {
+    let c = a.slice(-1);
+    let sub = a.slice(0, -1);
+    let code = c.charCodeAt();
+    if (code === 90) return sub + 'AA';
+    return sub + String.fromCharCode(code + 1);
+}
+
+function findColumnNumber(worksheet, name) {
+    if (!name) throw new Error('请输入需要的字段');
+    let startIndex = 'A';
+    let header = worksheet[startIndex + '1'];
+    let maxTry = 10000;
+    let currentTry = 1;
+    while (header.v !== name) {
+        startIndex = alphabetNext(startIndex);
+        header = worksheet[startIndex + '1'];
+        currentTry++;
+        if (currentTry >= maxTry) {
+            throw new Error(`该表格可能不存在${name} or 表格列数超过10000行`);
+        }
+    }
+    return {
+        index: currentTry - 1,
+        string: startIndex
+    };
+}
+
+const {
+    index: salerIndex,
+    string: salerColumn
+} = findColumnNumber(worksheet, salerName);
+const {
+    index: bussinessAddressIndex,
+    string: bussinessAddressColumn
+} = findColumnNumber(worksheet, bussinessAddress);
+const {
+    index: billAddressIndex,
+    string: billAddressColumn
+} = findColumnNumber(worksheet, billAddress);
+
+// 根据表格号获取行号和列号
+function splitColumnAndRow(value) {
+    let arr = value.split('');
+    let isColumn = true;
+    let column = '';
+    let row = '';
+    arr.forEach((item) => {
+        if (isNaN(Number(item))) {
+            column += item;
+        } else {
+            isColumn = false;
+        }
+        if (!isColumn) {
+            row += item
+        }
+    })
+    return {
+        column,
+        row: Number(row)
+    }
+
+}
+
+let currentRow = 1;
+let rows = [];
+let res = [];
+
+Object.keys(worksheet).forEach(item => {
+    if (item === '!ref') return;
+    const {
+        column,
+        row
+    } = splitColumnAndRow(item);
+    let {
+        w
+    } = worksheet[item];
+    // console.log(worksheet[item]);
+    if (currentRow !== row) {
+        currentRow = row;
+        let cannotUseFlag = rows.some((item, index) => {
+            if ((index === bussinessAddressIndex || index === billAddressIndex) && item.indexOf('测试') !== -1) {
+                return true;
+            }
+        });
+
+        if (!cannotUseFlag) {
+            res.push(rows);
+        };
+        rows = [];
+    }
+    if (column === salerColumn && row !== 1) {
+        let valueEn = w.split(',');
+        let valueZh = w.split('，');
+        let value = valueEn.length >= valueZh.length ? valueEn : valueZh;
+        w = value[0];
+    }
+    rows.push(w);
+});
+console.log(res);
--- readme.md
+++ readme.md
@@ -1,1 +1,16 @@
 write some simple tool
+
+## jsonp实现
+
+
+## event-emitter
+
+api
+- on
+- emit
+- off
+- once
+
+## 订阅发布模式
+
+## easy tapable
