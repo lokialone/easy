33eb4690f3aa7f4992ec6251f14a45db44abd422
loki <648875862@qq.com>
2019-10-28 10:57:42 +0800

    feat: 重构markdown test

--- packages/markdown/tests/tokenizer.test.ts
+++ packages/markdown/tests/tokenizer.test.ts
@@ -1,25 +1,21 @@
-import Tokenizer from '../src/tokenizer';
+import tokenizer from '../src/tokenizer';
 
 
 test('transform headerline', () => {
-    // let tokenizer = ;
-    let tokenizer1 = new Tokenizer('## header');
-    let tokenizer2 = new Tokenizer('## header ##');
-    let tokenizer3 = new Tokenizer('##header');
-
-    expect(new Tokenizer('# header')).toEqual([{tag: 'h1'}, {content: 'header'}]);
-    expect(tokenizer1)).toEqual([{tag: 'h2'}, {content: 'header'}]);
-    expect(tokenizer2)).toEqual([{tag: 'h2'}, {content: 'header ##'}]);
-    expect(tokenizer3)).toEqual([{content: '##header'}]);
+   
+    expect(tokenizer('# header')).toEqual([{tag: 'h1'}, {content: 'header'}]);
+    expect(tokenizer(('## header'))).toEqual([{tag: 'h2'}, {content: 'header'}]);
+    expect(tokenizer('## header ##')).toEqual([{tag: 'h2'}, {content: 'header ##'}]);
+    expect(tokenizer('##header')).toEqual([{content: '##header'}]);
 });
 
 test('transform ullist', () => {
-    expect(new Tokenizer('- header').getResult()).toEqual([{parent: 'ul', tag: 'li'}, {content: 'header'}]);
-    expect(new Tokenizer('header - hello').getResult()).toEqual([{"content": "header "}, {"parent": "ul", "tag": "li"}, {"content": "hello"}]);
+    expect(tokenizer('- header')).toEqual([{parent: 'ul', tag: 'li'}, {content: 'header'}]);
+    expect(tokenizer('header - hello')).toEqual([{"content": "header "}, {"parent": "ul", "tag": "li"}, {"content": "hello"}]);
 })
 
 test('transform orderlist', () => {
-    expect(new Tokenizer('1. hello').getResult()).toEqual([{tag: "li", parent: 'ol', content: '1.'}, {content: "hello"}]);
-    expect(new Tokenizer('22. hello')).toEqual([{tag: "li", parent: 'ol',  content: '22.'}, {content: "hello"}]);
-    expect(new Tokenizer('11.hello')).toEqual([{content: "11.hello"}]);
+    expect(tokenizer('1. hello')).toEqual([{tag: "li", parent: 'ol', content: '1.'}, {content: "hello"}]);
+    expect(tokenizer('22. hello')).toEqual([{tag: "li", parent: 'ol',  content: '22.'}, {content: "hello"}]);
+    expect(tokenizer('11.hello')).toEqual([{content: "11.hello"}]);
 })
