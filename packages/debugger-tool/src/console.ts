c3c20b90ee431a0d355e21c6782d5e91316a3728
loki <648875862@qq.com>
2019-11-19 15:53:00 +0800

    feat: basic console.ts

--- packages/debugger-tool/src/console.ts
+++ packages/debugger-tool/src/console.ts
@@ -1,5 +1,27 @@
-console.log('xxxxx');
-let app = document.getElementById('app');
-if (app){
-    app.innerHTML = 'xxxx';
-}
+let _console = window.console;
+let infos: any = [];
+window.console = null;
+const proxy = new Proxy(_console, {
+    get(target, property, ) {
+        if (property in target) {
+            if (typeof target[property] === 'function') {
+                return (message, ...args) =>  {
+                    infos.push({type: property, message, args});
+                    target[property](message, ...args);
+                }
+            } 
+            return target[property];
+        } else {
+            throw new Error(`${property} not in console`);
+        }
+    }
+});
+
+window.console = proxy;
+let infosProxy = new Proxy(infos, {
+    set(target, key, value) {
+        Reflect.set(target, key, value);
+    }
+})
+export default infos;
+
---
+++ packages/debugger-tool/src/global.d.ts
@@ -0,0 +1,20 @@
+
+declare global {
+    // interface Iconsole {
+    //     infos: any[],
+    //     log: Function,
+    //     error: Function,
+    //     warn: Function
+    // }
+    // interface Window {
+    //     console: Iconsole
+    // }
+    // interface Console {
+    //     infos: any[],
+    //     log: (message?: any, ...optionalParams: any[]) => {},
+    //     error: Function,
+    //     warn: Function
+    // }
+}
+
+export {}
